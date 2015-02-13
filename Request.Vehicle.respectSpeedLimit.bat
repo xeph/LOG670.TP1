ccb345a0981302816c8882001526716b1bb60cfb
Marc-Andre Destrempes <xeph_555@hotmail.com>
2015-02-13 12:40:16 -0500

    Ajout des fichiers de commandes Request

---
+++ Request.Vehicle.respectSpeedLimit.bat
@@ -0,0 +1,4 @@
+-- Vehicle.respectSpeedLimit
+?ve1.respectSpeedLimit(70)
+!set ve1.Speed := 100
+?ve1.respectSpeedLimit(70)
---
+++ Request.Vehicle.vehicleNeedRepair.bat
@@ -0,0 +1,4 @@
+-- Vehicle.vehicleNeedRepair
+?ve1.vehicleNeedRepair()
+!set ch1.isOk := false
+?ve1.vehicleNeedRepair()
